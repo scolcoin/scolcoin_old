d6d1252306863ba22d4fba7a63e73d7c5f3bb7df
Social Coin of Colombia (SCOL) <33010917+scolcoin@users.noreply.github.com>
2020-09-28 15:06:03 -0500

    Update atomic_pointer.h
    Fix ARM 64

--- src/leveldb/port/atomic_pointer.h
+++ src/leveldb/port/atomic_pointer.h
@@ -36,6 +36,8 @@
 #define ARCH_CPU_X86_FAMILY 1
 #elif defined(__ARMEL__)
 #define ARCH_CPU_ARM_FAMILY 1
+#elif defined(__aarch64__)
+#define ARCH_CPU_ARM64_FAMILY 1
 #elif defined(__ppc__) || defined(__powerpc__) || defined(__powerpc64__)
 #define ARCH_CPU_PPC_FAMILY 1
 #endif
@@ -49,6 +51,14 @@ namespace port {
 // windows.h already provides a MemoryBarrier(void) macro
 // http://msdn.microsoft.com/en-us/library/ms684208(v=vs.85).aspx
 #define LEVELDB_HAVE_MEMORY_BARRIER
+
+// ARM64
+#elif defined(ARCH_CPU_ARM64_FAMILY)
+inline void MemoryBarrier() {
+  asm volatile("dmb sy" : : : "memory");
+}
+#define LEVELDB_HAVE_MEMORY_BARRIER
+ 
 
 // Mac OS
 #elif defined(OS_MACOSX)
@@ -216,6 +226,7 @@ class AtomicPointer {
 #undef LEVELDB_HAVE_MEMORY_BARRIER
 #undef ARCH_CPU_X86_FAMILY
 #undef ARCH_CPU_ARM_FAMILY
+#undef ARCH_CPU_ARM64_FAMILY  
 #undef ARCH_CPU_PPC_FAMILY
 
 }  // namespace port
